9ba13d6c035049e9ebbf3c6bc1422d15c0bdde7b
roey_averbuch <roey6922@gmail.com>
2026-01-12 17:42:19 +0200

    adding service func in the routes

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -1,15 +1,22 @@
 from fastapi import APIRouter
 from schemas import IpData
+from services import *
 
 locations_router = APIRouter(
     prefix="/locations",
     tags=["Locations Storage"])
 
+rh = RedisHandler()
 
 @locations_router.GET("/")
 def get_all_data():
-    pass
+    result = rh.fetch_all()
+
+    return result if result else {"error"}
 
 locations_router.post("/")
 def insert_data(data:IpData):
-    pass
+    success = rh.insert(data)
+    if not success:
+        return {"error": "Failed to save to Redis"}
+    return {"message": f"Data for {str(data.ip)} stored successfully"}
