ed0132751aa5f42c2bca66bca68034d4d34a214d
roey_averbuch <roey6922@gmail.com>
2026-01-12 16:30:19 +0200

    main app.py

--- service-b/app/app.py
+++ service-b/app/app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from routes import locations_router
-from schemas import *
+import uvicorn
 
 
 app = FastAPI()
 
 @app.get("/")
-def health():
-    return {"Message":"GET /locations return all redis storege , POST /locations get {ip:coordinates}"}
+def root():
+    return {"Message":"GET /locations return all redis storege , POST /locations send {ip:coordinates}"}
 
 app.include_router(locations_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
