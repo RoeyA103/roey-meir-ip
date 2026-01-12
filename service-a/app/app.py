dbc665ae22d84d8010ae142d4db79d7b24139e59
meyir moshe <mma088651259@gmail.com>
2026-01-12 17:47:57 +0200

    Small update in uvicorn running

--- service-a/app/app.py
+++ service-a/app/app.py
@@ -12,4 +12,4 @@ app.include_router(router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost",port=8000 , reload=True)
+    uvicorn.run("app:app", host="localhost",port=8000 , reload=True)
