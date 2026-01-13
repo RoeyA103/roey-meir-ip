42fbb8cd9ed0c1a7239f882120bfe460e0d2f6b3
roey_averbuch <roey6922@gmail.com>
2026-01-13 11:13:11 +0200

    adding env to req + router

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter
 from schemas import IpData
 from services import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+
+db_host = os.getenv("DATABASE_HOST")
+db_port = os.getenv("DATABASE_PORT",6379)
 
 locations_router = APIRouter(
     prefix="/locations",
     tags=["Locations Storage"])
 
-rh = RedisHandler(host="redis",port=6379)
+rh = RedisHandler(host=db_host,port=db_port)
 
 @locations_router.get("/")
 def get_all_data():
